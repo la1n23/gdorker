af26c93b33fbbd0f96f8c3f415e66980486d4cac
la1n23 <la1n23@proton.me>
2025-04-10 03:03:44 +0300

    ++

--- g-dorker.py
+++ g-dorker.py
@@ -16,6 +16,7 @@ init(autoreset=True)
 
 class Logger:
     def __init__(self, options: Dict[str, Any]) -> None:
+        self.options = options
         self.has_title = options.get('title', False)
         self.has_code = options.get('code', False)
         self.has_body = options.get('body', False)
@@ -91,7 +92,7 @@ class GoogleSearch:
         self.logger = logger
         self.api_key = api_key
         self.cse_id = cse_id
-        self.offset: int = 1
+        self.offset: int = 0
         self.is_limit_reached = False
 
     @property
@@ -133,22 +134,23 @@ class GoogleSearch:
                 }
             self.logger.log_url(pieces)
 
-    def query_results(self, query: str) -> None:
-        self.offset: int = 1
-        start: int = self.offset
+    def query_results(self, query: str, offset: int | None) -> None:
+        if offset is None:
+            offset = 0
+        self.offset: int = offset
+        self.query = query
         while not self.is_limit_reached:
             self.logger.log_info(f"Page {self._page}")
-            results = self._google_search(query, start=start)
+            results = self._google_search(query, start=self.offset)
             self._print_results(results)
             if not results:
                 break
-            self.offset = start + self.per_page
-            start += self.per_page
+            self.offset = self.offset + self.per_page
             time.sleep(1)
 
-def handle_exit_signal(google_search: GoogleSearch, logger: Logger, signum: int, frame: Any) -> NoReturn:
+def handle_exit_signal(session, api_key, cse_id, file_or_query, logger: Logger, gs: GoogleSearch) -> NoReturn:
     logger.log_info("Interrupted by user, saving session...")
-    #google_search.save_session()  # TODO
+    session.save(api_key, cse_id, file_or_query, logger, gs)
     exit(0)
 
 def load_queries(file_or_query: str) -> list[str]:
@@ -159,17 +161,67 @@ def load_queries(file_or_query: str) -> list[str]:
     except Exception as e:
         return [file_or_query]
 
-def main(api_key: str, cse_id: str, file_or_query: str, f_options) -> None:
+
+class Session():
+    """
+    todo: save data inside session class and pass it google search
+    """
+    def __init__(self, file):
+        self.file = file
+
+    def load(self):
+        with open(self.file, "r") as f:
+            j = json.loads(f.read())
+            return j
+
+    def clean(self):
+        os.remove(self.file)
+
+    def save(self, api_key, cse_id, file_or_query, logger: Logger, gs: GoogleSearch):
+        session = {
+            'api_key': api_key,
+            'cse_id': cse_id,
+            'file_or_query': file_or_query,
+            'f_options': logger.options,
+            'current_query': gs.query,
+            'offset': gs.offset
+        }
+        j = json.dumps(session, indent=2)
+        with open('./session.json', 'w') as f:
+            f.write(j)    
+
+def main(api_key: str, cse_id: str, file_or_query: str, resume: bool, f_options) -> None:
+    """
+    todo: take api keys form config file
+    """
+    current_query = None
+    offset = None
+    session = Session("./session.json")
+    if resume:
+        data = session.load()
+        api_key = data['api_key']
+        cse_id = data['cse_id']
+        file_or_query = data['file_or_query']
+        f_options = data['f_options']
+        offset = data['offset']
+        current_query = data['current_query']
+
     logger = Logger(f_options)
     google_search = GoogleSearch(logger, api_key, cse_id)
 
-    signal.signal(signal.SIGINT, lambda signum, frame: handle_exit_signal(google_search, logger, signum, frame))
     queries = load_queries(file_or_query)
+    if current_query:
+        index = next((i for i, q in enumerate(queries) if q == current_query), len(queries))
+        queries = queries[index:]
+
+    signal.signal(signal.SIGINT, lambda signum, frame: handle_exit_signal(session, api_key, cse_id, file_or_query, logger, google_search))
     for query in queries:
         logger.log_info(f"Query: {query}")
-        google_search.query_results(query)
+        google_search.query_results(query, offset)
         if google_search.is_limit_reached:
-            break
+            session.save(api_key, cse_id, file_or_query, logger, google_search)
+            exit(1)
+    session.clean()
 
 if __name__ == "__main__":
     sys.stderr.write("""
@@ -195,22 +247,25 @@ Examples:
     ./g-dorker.py -q "site:example.com" -k YOUR_API_KEY -x YOUR_CSE_ID
     ./g-dorker.py -q "inurl:admin" -k YOUR_API_KEY -x YOUR_CSE_ID -t -c -b
     ./g-dorker.py -q "filetype:pdf" -k YOUR_API_KEY -x YOUR_CSE_ID -f results.txt -b
+    ./g-dorker.py -r
 """)
 
     required = parser.add_argument_group('Required arguments')
     required.add_argument(
+            "-r", "--resume",
+            action="store_true",
+            help="Resume previously interrupted session from ./session.json file. Other options will be ignored"
+    )
+    required.add_argument(
         "-q", "--query",
-        required=True,
         help="Search query (Google dork string) or path to file with queries"
     )
     required.add_argument(
         "-k", "--api-key",
-        required=True,
         help="Google Custom Search API key"
     )
     required.add_argument(
         "-x", "--cx",
-        required=True,
         help="Google Custom Search Engine ID"
     )
 
@@ -244,4 +299,4 @@ Examples:
         'code': args.code,
         'dest': args.file,
     }
-    main(args.api_key, args.cx, args.query, f_options)
+    main(args.api_key, args.cx, args.query, args.resume, f_options)
