8dbd4c89ce9bbc3f89e6f98d55439dc20b683313
la1n23 <la1n23@proton.me>
2025-04-10 07:59:38 +0300

    ++

--- g-dorker.py
+++ g-dorker.py
@@ -14,22 +14,27 @@ import re
 
 init(autoreset=True)
 
-class Logger:
-    def __init__(self, options: Dict[str, Any]) -> None:
-        self.options = options
-        self.has_title = options.get('title', False)
-        self.has_code = options.get('code', False)
-        self.has_body = options.get('body', False)
-        self.dest = options.get('dest', False)
-        self.is_extended = any([self.has_code, self.has_body])
-        if self.dest and isinstance(self.dest, str):
-            self.dest = open(self.dest, 'a')
+banner = """
+⠀⠀⣠⣶⣶⣦⣤⡄⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⣠⣤⣤⣤⣄⠀⠀⠀⣤⣤⣤⣤⣤⣀⡀⠀⠀⣀⣀⣤⡀⢠⣤⣤⣤⠀⣀⣀⣀⣤⣤⣤⣤⡄⠀⢠⣤⣤⣤⣤⣄⣀⠀⠀
+⠀⣼⡟⠁⠀⠉⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠉⠙⠻⣷⡄⠀⠀⣼⡿⠁⠀⠀⢻⡆⠀⠀⠘⣿⠀⠀⠈⢹⣷⠀⠀⠈⢿⡏⠀⣨⡿⠋⠀⠀⠈⣿⡏⠉⢉⠙⣿⡇⠀⠀⢻⡏⠀⠀⠉⣻⡇⠀
+⠀⣿⡇⠀⢠⣦⣼⣷⣦⠄⠀⠀⠀⠀⠀⠀⢸⣧⠀⠀⠀⠸⣷⠀⠀⣿⠇⠀⠀⠀⠀⣿⠀⠀⠀⣿⡶⠶⣾⡟⠁⠀⠀⠀⢸⣀⣴⣿⡀⠀⠀⠀⠀⢸⣷⣶⣿⡇⠈⠀⠀⠀⠸⣷⠶⢶⣾⠟⠀⠀
+⠀⢺⣇⠀⠀⠀⢹⣿⠀⠸⠿⠿⠾⠿⠗⠀⢘⣿⠀⠀⠀⢠⡿⠀⠀⣿⡆⠀⠀⠀⢠⡟⠀⠀⠀⢹⡁⠀⢸⣧⠀⠀⠀⠀⢸⠟⠁⠙⢷⡀⠀⠀⠀⢸⡇⠀⠙⠃⢠⣤⠀⠀⠀⣏⠀⠀⢻⣆⠀⠀
+⠀⠈⠻⣷⣶⣴⣿⡿⠀⠀⠀⠀⠀⠀⠀⣤⣼⣿⣤⣠⣴⠿⠁⠀⠀⠘⠿⣦⣤⣤⠟⠀⠀⠀⢠⣿⣧⣄⢀⣿⣷⣤⠀⣠⣽⣄⡀⢀⣠⣷⣤⠀⣠⣿⣷⣤⣴⣴⣾⣿⠀⠀⢴⣿⣦⡀⣸⣿⣦⡄
+                               
+                               By La1n
 
-    def f_title(self, title: str) -> str:
+"""
+
+class Formatter:
+    def __init__(self, options):
+        self.options = options
+        self.is_extended = any([self.options['code'], self.options['body']])
+        pass
+    def _title(self, title: str) -> str:
         return f"{Fore.MAGENTA}[{title}]"
-    def f_url(self, url: str) -> str:
+    def _url(self, url: str) -> str:
         return f"{Fore.GREEN}[{url}]"
-    def f_code(self, code: int) -> str:
+    def _code(self, code: int) -> str:
         color = Fore.WHITE
         if code >= 200 and code < 300:
             color = Fore.GREEN
@@ -38,54 +43,69 @@ class Logger:
         elif code >= 400 and code < 600:
             color = Fore.RED
         return f"{color}[{code}]"
-    def f_body(self, html: str) -> str:
+    def _body(self, html: str) -> str:
         soup = BeautifulSoup(html,'html.parser')
         body = soup.body.get_text() if soup.body else ""
         text = re.sub(r'[\s]+', " ", body).strip()[0:100]
         return f"{Fore.CYAN}[{text}]"
 
-    def f_result(self, info: str) -> str:
-        return f"{Fore.BLUE}[URL] {info}"
-    def f_info(self, info: str) -> str:
+    def debug(self, info: str) -> str:
+        return f"{Fore.LIGHTBLACK_EX}[DEBUG] {info}"
+    def info(self, info: str) -> str:
         return f"{Fore.BLUE}[INFO] {info}"
-    def f_error(self, error: str) -> str:
+    def error(self, error: str) -> str:
         return f"{Fore.RED}[ERROR] {error}"
+    def _result(self, info: str) -> str:
+        return f"{Fore.BLUE}[URL] {info}"
+    def result(self, pieces: Dict[str, str]):
+        output = [self._url(pieces["url"])]
 
-    def _build_result(self, pieces: Dict[str, str]):
-        output = [self.f_url(pieces["url"])]
+        if self.options['code']:
+            output.append(self._code(pieces["code"]))
+        if self.options['title']:
+            output.append(self._title(pieces['title']))
+        if self.options['body']:
+            output.append(self._body(pieces["body"]))
+        return self._result(' '.join(output))
 
-        if self.has_code:
-            output.append(self.f_code(pieces["code"]))
-        if self.has_title:
-            output.append(self.f_title(pieces['title']))
-        if self.has_body:
-            output.append(self.f_body(pieces["body"]))
-        return ' '.join(output)
 
-    def write(self, line: str) -> None:
+class Logger:
+    def __init__(self, options: Dict[str, Any]) -> None:
+        self.dest = options['dest']
+        self.debug_enabled = options['debug']
+        self.formatter = Formatter(options)
+        if self.dest and isinstance(self.dest, str):
+            self.dest = open(self.dest, 'a')
+
+    def _write(self, line: str) -> None:
         print(line)
 
-    def log_url(self, pieces: Dict[str, str]) -> None:
-        result = self._build_result(pieces)
-        line = self.f_info(result)
-        self.write(line)
+    def url(self, pieces: Dict[str, str]) -> None:
+        line = self.formatter.result(pieces)
+        self._write(line)
         if self.dest:
             print(line, file=self.dest)
 
-    def log_info(self, info: str) -> None:
-        line = self.f_info(info)
-        self.write(line)
+    def info(self, info: str) -> None:
+        line = self.formatter.info(info)
+        self._write(line)
 
-    def log_error(self, error: str) -> None:
-        line = self.f_error(error)
-        self.write(line)
+    def error(self, error: str) -> None:
+        line = self.formatter.error(error)
+        self._write(line)
+
+    def debug(self, info: str) -> None:
+        if not self.debug_enabled:
+            return
+        line = self.formatter.debug(info)
+        self._write(line)
 
     def __exit__(self, exc_type, exc_value, traceback):
         if self.dest:
             self.dest.close()
 
 
-class GoogleSearch:
+class Dorker:
     per_page = 10
 
     def __init__(self, logger: Logger, api_key: str, cse_id: str) -> None:
@@ -94,28 +114,29 @@ class GoogleSearch:
         self.cse_id = cse_id
         self.offset: int = 0
         self.is_limit_reached = False
+        self.client = lambda q, cx, start, num: build("customsearch", "v1", developerKey=self.api_key).cse().list(q=q, cx=cx, start=start, num=num).execute()
 
     @property
     def _page(self) -> int:
         return self.offset // self.per_page + 1
 
-    def _google_search(self, query: str, start: int = 0) -> List[Dict[str, Any]]:
-        service = build("customsearch", "v1", developerKey=self.api_key)
+    def _search(self, query: str, start: int = 0) -> List[Dict[str, Any]]:
         try:
-            res = service.cse().list(q=query, cx=self.cse_id, start=start, num=self.per_page).execute()
+            res = self.client(query, self.cse_id, start, self.per_page)
             items = res.get('items')
             return items
         except Exception as e:
             try:
+                self.logger.debug(e)
                 error_content = json.loads(e.content)
                 #print(json.dumps(error_content, indent=4))
                 if error_content['error']['status'] == 'RESOURCE_EXHAUSTED':
-                    self.logger.log_info("Resource limit reached. Please try again in 24h")
+                    self.logger.info("Resource limit reached. Please try again in 24h")
                     self.is_limit_reached = True
                 else:
-                    self.logger.log_error(e.content)
+                    self.logger.error(e.content)
             except json.JSONDecodeError:
-                self.logger.log_error(e.content)
+                self.logger.error(e.content)
             return []
 
     def _print_results(self, results: list):
@@ -132,7 +153,7 @@ class GoogleSearch:
                     "code": res.status_code,
                     "body": res.text
                 }
-            self.logger.log_url(pieces)
+            self.logger.url(pieces)
 
     def query_results(self, query: str, offset: int | None) -> None:
         if offset is None:
@@ -140,27 +161,13 @@ class GoogleSearch:
         self.offset: int = offset
         self.query = query
         while not self.is_limit_reached:
-            self.logger.log_info(f"Page {self._page}")
-            results = self._google_search(query, start=self.offset)
+            self.logger.info(f"Page {self._page}")
+            results = self._search(query, start=self.offset)
             self._print_results(results)
             if not results:
                 break
             self.offset = self.offset + self.per_page
             time.sleep(1)
-
-def handle_exit_signal(session, api_key, cse_id, file_or_query, logger: Logger, gs: GoogleSearch) -> NoReturn:
-    logger.log_info("Interrupted by user, saving session...")
-    session.save(api_key, cse_id, file_or_query, logger, gs)
-    exit(0)
-
-def load_queries(file_or_query: str) -> list[str]:
-    try:
-        with open(file_or_query, 'r') as f:
-            queries = f.readlines()
-            return queries
-    except Exception as e:
-        return [file_or_query]
-
 
 class Session():
     """
@@ -177,37 +184,53 @@ class Session():
     def clean(self):
         os.remove(self.file)
 
-    def save(self, api_key, cse_id, file_or_query, logger: Logger, gs: GoogleSearch):
+    def save(self, api_key, cse_id, file_or_query, logger: Logger, gs: Dorker):
         session = {
             'api_key': api_key,
             'cse_id': cse_id,
             'file_or_query': file_or_query,
-            'f_options': logger.options,
+            'options': logger.formatter.options,
             'current_query': gs.query,
             'offset': gs.offset
         }
         j = json.dumps(session, indent=2)
-        with open('./session.json', 'w') as f:
+        with open(self.file, 'w') as f:
             f.write(j)    
 
-def main(api_key: str, cse_id: str, file_or_query: str, resume: bool, f_options) -> None:
+def handle_exit_signal(session, api_key, cse_id, file_or_query, logger: Logger, gs: Dorker) -> NoReturn:
+    logger.info("Interrupted by user, saving session...")
+    session.save(api_key, cse_id, file_or_query, logger, gs)
+    exit(0)
+
+def load_queries(file_or_query: str) -> list[str]:
+    try:
+        with open(file_or_query, 'r') as f:
+            queries = f.readlines()
+            unique = list(set(queries))
+            return queries
+    except Exception as e:
+        return [file_or_query]
+
+
+def main(api_key: str, cse_id: str, file_or_query: str, resume: bool, session, options) -> None:
     """
     todo: take api keys form config file
+    todo: async requests
     """
     current_query = None
     offset = None
-    session = Session("./session.json")
+    session = Session(session)
     if resume:
         data = session.load()
         api_key = data['api_key']
         cse_id = data['cse_id']
         file_or_query = data['file_or_query']
-        f_options = data['f_options']
+        options = data['options']
         offset = data['offset']
         current_query = data['current_query']
 
-    logger = Logger(f_options)
-    google_search = GoogleSearch(logger, api_key, cse_id)
+    logger = Logger(options)
+    google_search = Dorker(logger, api_key, cse_id)
 
     queries = load_queries(file_or_query)
     if current_query:
@@ -216,7 +239,7 @@ def main(api_key: str, cse_id: str, file_or_query: str, resume: bool, f_options)
 
     signal.signal(signal.SIGINT, lambda signum, frame: handle_exit_signal(session, api_key, cse_id, file_or_query, logger, google_search))
     for query in queries:
-        logger.log_info(f"Query: {query}")
+        logger.info(f"Query: {query}")
         google_search.query_results(query, offset)
         if google_search.is_limit_reached:
             session.save(api_key, cse_id, file_or_query, logger, google_search)
@@ -224,16 +247,7 @@ def main(api_key: str, cse_id: str, file_or_query: str, resume: bool, f_options)
     session.clean()
 
 if __name__ == "__main__":
-    sys.stderr.write("""
-⠀⠀⣠⣶⣶⣦⣤⡄⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⣠⣤⣤⣤⣄⠀⠀⠀⣤⣤⣤⣤⣤⣀⡀⠀⠀⣀⣀⣤⡀⢠⣤⣤⣤⠀⣀⣀⣀⣤⣤⣤⣤⡄⠀⢠⣤⣤⣤⣤⣄⣀⠀⠀
-⠀⣼⡟⠁⠀⠉⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠉⠙⠻⣷⡄⠀⠀⣼⡿⠁⠀⠀⢻⡆⠀⠀⠘⣿⠀⠀⠈⢹⣷⠀⠀⠈⢿⡏⠀⣨⡿⠋⠀⠀⠈⣿⡏⠉⢉⠙⣿⡇⠀⠀⢻⡏⠀⠀⠉⣻⡇⠀
-⠀⣿⡇⠀⢠⣦⣼⣷⣦⠄⠀⠀⠀⠀⠀⠀⢸⣧⠀⠀⠀⠸⣷⠀⠀⣿⠇⠀⠀⠀⠀⣿⠀⠀⠀⣿⡶⠶⣾⡟⠁⠀⠀⠀⢸⣀⣴⣿⡀⠀⠀⠀⠀⢸⣷⣶⣿⡇⠈⠀⠀⠀⠸⣷⠶⢶⣾⠟⠀⠀
-⠀⢺⣇⠀⠀⠀⢹⣿⠀⠸⠿⠿⠾⠿⠗⠀⢘⣿⠀⠀⠀⢠⡿⠀⠀⣿⡆⠀⠀⠀⢠⡟⠀⠀⠀⢹⡁⠀⢸⣧⠀⠀⠀⠀⢸⠟⠁⠙⢷⡀⠀⠀⠀⢸⡇⠀⠙⠃⢠⣤⠀⠀⠀⣏⠀⠀⢻⣆⠀⠀
-⠀⠈⠻⣷⣶⣴⣿⡿⠀⠀⠀⠀⠀⠀⠀⣤⣼⣿⣤⣠⣴⠿⠁⠀⠀⠘⠿⣦⣤⣤⠟⠀⠀⠀⢠⣿⣧⣄⢀⣿⣷⣤⠀⣠⣽⣄⡀⢀⣠⣷⣤⠀⣠⣿⣷⣤⣴⣴⣾⣿⠀⠀⢴⣿⣦⡀⣸⣿⣦⡄
-                               
-                               By La1n
-
-""")
+    print(banner)
     parser = argparse.ArgumentParser(
         description="""
 G-Dorker - Google Custom Search Engine Dorking Tool
@@ -247,15 +261,11 @@ Examples:
     ./g-dorker.py -q "site:example.com" -k YOUR_API_KEY -x YOUR_CSE_ID
     ./g-dorker.py -q "inurl:admin" -k YOUR_API_KEY -x YOUR_CSE_ID -t -c -b
     ./g-dorker.py -q "filetype:pdf" -k YOUR_API_KEY -x YOUR_CSE_ID -f results.txt -b
-    ./g-dorker.py -r
+    ./g-dorker.py -q ./bb_dorks.txt -k YOUR_API_KEY -x YOUR_CSE_ID -f results.txt -b
+    ./g-dorker.py -q ./bb_dorks.txt -k YOUR_API_KEY -x YOUR_CSE_ID -f results.txt --session ./g_dorker_session_1744260850.json
 """)
 
     required = parser.add_argument_group('Required arguments')
-    required.add_argument(
-            "-r", "--resume",
-            action="store_true",
-            help="Resume previously interrupted session from ./session.json file. Other options will be ignored"
-    )
     required.add_argument(
         "-q", "--query",
         help="Search query (Google dork string) or path to file with queries"
@@ -279,6 +289,15 @@ Examples:
         action="store_true",
         help="Include page title"
     )
+    output.add_argument(
+        "-d", "--debug",
+        action="store_true",
+        help="Debug output"
+    )
+    output.add_argument(
+        "-s", "--session",
+        help="Resume from"
+    )
 
     content = parser.add_argument_group('Options that trigger additional request for each URL')
     content.add_argument(
@@ -293,10 +312,18 @@ Examples:
     )
 
     args = parser.parse_args()
-    f_options = {
+    options = {
         'title': args.title,
         'body': args.body,
         'code': args.code,
         'dest': args.file,
+        'debug': args.debug
     }
-    main(args.api_key, args.cx, args.query, args.resume, f_options)
+    if args.debug:
+        print(dir(Fore))
+
+    session = args.session
+    resume = bool(session)
+    if not session:
+        session = f"g_dorker_session_{int(time.time())}.json"
+    main(args.api_key, args.cx, args.query, resume, session, options)
